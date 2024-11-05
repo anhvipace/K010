5eb76e750837bd4adc5e39d7836faa9ab2c80b30
Nguyễn Duy Hoàng Anh <anhvipacech@gmail.com>
2024-11-05 17:07:20 +0700

    05/11/2024

---
+++ LoginApp/LoginWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+
+namespace LoginApp
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Password;
+
+            // Kiểm tra thông tin đăng nhập
+            if (username == "admin" && password == "123456")
+            {
+                MessageBox.Show("Đăng nhập thành công!");
+                // Thực hiện các thao tác sau khi đăng nhập thành công
+            }
+            else
+            {
+                MessageBox.Show("Tên đăng nhập hoặc mật khẩu không đúng.");
+            }
+        }
+
+        private void BtnForgotPassword_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Chức năng quên mật khẩu đang được phát triển.");
+        }
+
+        private void BtnCreateAccount_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Chức năng tạo tài khoản mới đang được phát triển.");
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
